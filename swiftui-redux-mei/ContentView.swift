6f8b61be363a1cc584044a67c76f6a09be0319d3
Meidiana Monica <meimei_monica@yahoo.com>
2020-05-20 11:21:52 +0700

    project empty

---
+++ swiftui-redux-mei/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-redux-mei
+//
+//  Created by Meidiana Monica on 20/05/20.
+//  Copyright © 2020 Meidiana Monica. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
